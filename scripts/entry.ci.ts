cba8b716e9a11f5a62caa4ca7186f2ffc87f17a1
ecumene <mitch@spellbook.legal>
2024-11-27 22:09:06 -0330

    Actually commit

--- scripts/entry.ci.ts
+++ scripts/entry.ci.ts
@@ -2,5 +2,5 @@ import { getRepoIssuesAndPRs } from "./github-api.ci";
 import { summarizeIssuesAndPRs } from "./tech-lead.ci";
 
 getRepoIssuesAndPRs().then((issuesAndPRs) => {
-  summarizeIssuesAndPRs(issuesAndPRs.issues, issuesAndPRs.prs);
+  console.log(summarizeIssuesAndPRs(issuesAndPRs.issues, issuesAndPRs.prs));
 });
