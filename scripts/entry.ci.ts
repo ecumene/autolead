8bdc82f8d24f34589305835c0720e4792768e673
ecumene <mitch@spellbook.legal>
2024-11-27 22:09:56 -0330

    Oh come on

--- scripts/entry.ci.ts
+++ scripts/entry.ci.ts
@@ -1,6 +1,7 @@
 import { getRepoIssuesAndPRs } from "./github-api.ci";
 import { summarizeIssuesAndPRs } from "./tech-lead.ci";
 
-getRepoIssuesAndPRs().then((issuesAndPRs) => {
-  console.log(summarizeIssuesAndPRs(issuesAndPRs.issues, issuesAndPRs.prs));
-});
+const githubIssuesAndPRs = await getRepoIssuesAndPRs();
+console.log(
+  await summarizeIssuesAndPRs(githubIssuesAndPRs.issues, githubIssuesAndPRs.prs)
+);
